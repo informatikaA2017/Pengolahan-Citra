14603909987fda2e5dc1139152aafa5963a6c96e
Zakky Muhammad Fajar <zakkymf@gmail.com>
2020-05-16 06:05:06 +0700

    update basic.py

--- basic.py
+++ basic.py
@@ -3,11 +3,24 @@ import numpy as np
 
 img = cv2.imread('./images/messi.jpg')
 
+#Access Pixel Value
 px = img[100,100]
 print(px)
 
+# accessing only blue pixel
 blue = img[100,100,0]
 print(blue)
+
+# accessing RED value
+red = img.item(10,10,2)
+print(red)
+
+# modifying RED value
+img.itemset((10,10,2),100)
+img.item(10,10,2)
+
+print(img.shape)
+print(img.size)
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
