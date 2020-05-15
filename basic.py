8359ac1e4ecd04eceec1a35edc376237c50609e9
Zakky Muhammad Fajar <zakkymf@gmail.com>
2020-05-16 05:46:22 +0700

    edit name file, add pixel value

--- basic.py
+++ basic.py
@@ -3,6 +3,12 @@ import numpy as np
 
 img = cv2.imread('./images/messi.jpg')
 
+px = img[100,100]
+print(px)
+
+blue = img[100,100,0]
+print(blue)
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
